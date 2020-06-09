cb9cfc862e1082febedbc5de69744193a70d79e9
Madhu.Santhoshkumar <Madhu.Santhoshkumar@MLBMYSL-106608.in.marlabs.com>
2020-06-09 10:56:28 -0400

    test

---
+++ ls/src/test/java/testdriver/TestRunner.java
@@ -0,0 +1,16 @@
+package test.java.testdriver;
+
+import org.xframium.device.ng.TestContainer;
+import org.xframium.driver.TestDriver;
+
+import com.ul.ls.pages.Login;
+import com.ul.ls.pages.spi.LoginImpl;
+
+public class TestRunner {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		TestDriver.main(new String[] {"../ls/src/com/ul/ls/resources/driverConfigTXT.txt"});
+	}
+
+}
